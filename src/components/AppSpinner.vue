c6105f13d49e66154db90f9a53d665e01dab94a2
soniabergamini <sounia96@outlook.it>
2023-08-29 12:55:55 +0200

    feat(component): add spinner loader

---
+++ src/components/AppSpinner.vue
@@ -0,0 +1,31 @@
+<script>
+import { store } from '../data/store';
+
+export default {
+    name: 'AppSpinner',
+    data() {
+        return {
+            store
+        }
+    },
+}
+
+</script>
+
+<template>
+    <div v-if="store.loading">
+        <button class="btn" type="button" disabled>
+            <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
+            Loading...
+        </button>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use 'src/style.scss' as *;
+
+button {
+    background-color: $light-orange;
+    border-color: $light-orange;
+}
+</style>
