649b5aea5b027648b33454cf6b505b6ea92c51a1
soniabergamini <sounia96@outlook.it>
2023-08-29 14:26:00 +0200

    style(spinner): improve color

--- src/components/AppSpinner.vue
+++ src/components/AppSpinner.vue
@@ -13,8 +13,8 @@ export default {
 </script>
 
 <template>
-    <div v-if="store.loading">
-        <button class="btn" type="button" disabled>
+    <div v-show="store.loading" class="p-4">
+        <button class="btn btn-loading text-white px-2 py-1" type="button" disabled>
             <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
             Loading...
         </button>
@@ -24,7 +24,7 @@ export default {
 <style scoped lang="scss">
 @use 'src/style.scss' as *;
 
-button {
+.btn-loading {
     background-color: $light-orange;
     border-color: $light-orange;
 }
