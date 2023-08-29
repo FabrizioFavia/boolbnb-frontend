e888ba1bd1ae20dd1f4019b4d70dacc76a8e3a50
FabrizioFavia <personal.favia@gmail.com>
2023-08-29 12:55:03 +0200

    feat(AppHeader): improve navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,23 +10,75 @@ export default {
 </script>
 
 <template>
-    <nav class="navbar bg-white">
+    <nav class="navbar navbar-expand-lg bg-white justify-content-between">
         <div class="container">
-            <a class="navbar-brand"><img class="navLogo" src="../assets/logo2.jpeg" alt=""></a>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+            <div class="logoContainer">
+                <router-link class="" to="/homeProva">
+                    <img class="navLogo" src="../assets/logo2.jpeg" alt="">
+                </router-link>
+                <router-link class="" to="/homeProva">
+                    <img class="smallLogo" src="../assets/logo.jpeg" alt="">
+                </router-link>
+            </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="searchContainer d-flex justify-content-center w-100">
+                    <form class="d-flex w-75">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    </form>
+                </div>
+                <div class="linkContainer">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Register</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </nav>
 </template>
+
+
+
+
 
 <style scoped lang="scss">
 @use 'src/style.scss' as *;
 
 
-.navLogo{
+.navLogo {
     height: 80px;
+}
+
+/* MEDIA QUERY */
+
+@media (min-width: 576px) {
+    .smallLogo {
+        display: none;
+    }
+}
+
+@media (max-width: 576px) {
+    .navLogo {
+        display: none;
+    }
+
+    .smallLogo {
+        display: block;
+        max-height: 80px;
+        margin-top: 0;
+    }
+
+    .searchContainer{
+        margin-top: 20px;
+    }
 }
 </style>
 
